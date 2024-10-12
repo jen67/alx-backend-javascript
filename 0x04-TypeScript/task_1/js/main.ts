730a273bcb07ba24c776941c2c2728ffd92ffb95
jen67 <amakrigift2000@gmail.com>
2024-10-12 21:36:32 +0100

    taskk 2 done

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -20,3 +20,16 @@ const teacher3: BaseTeacher = {
 };
 
 console.log(teacher3);
+
+interface Directors extends Teacher{
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
