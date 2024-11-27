105976eb0b83a0406bb88e4bdfb81aee22e22560
jen67 <amakrigift2000@gmail.com>
2024-11-27 14:12:08 +0000

    task 5 done

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  const { url } = req;
+
+  if (url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+    countStudents('database.csv')
+      .then((message) => {
+        res.end(message);
+      })
+      .catch((err) => {
+        res.end(err.message);
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
+
